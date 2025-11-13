fa6389441c810340a623086c89eb11556c43570f
borisdvlpr <eduffmiranda@hotmail.com>
2025-11-13 18:43:20 +0000

    First blood

---
+++ src/main/java/com/borisdvlpr/ticketline/SpringBootTicketlineApplication.java
@@ -0,0 +1,13 @@
+package com.borisdvlpr.ticketline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTicketlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTicketlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/borisdvlpr/ticketline/SpringBootTicketlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borisdvlpr.ticketline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTicketlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
