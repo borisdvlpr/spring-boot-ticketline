80d84c4349f70f49d345651fe8cffdd782c0019f
borisdvlpr <eduffmiranda@hotmail.com>
2025-11-17 22:52:15 +0000

    build: bootstrap project

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,16 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql:42.7.8")
+    implementation("org.projectlombok:lombok:1.18.42")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.h2database:h2:2.3.232")
 }
 
 tasks.withType<Test> {
