5f99c63b849fde0bc56ea4e7b371f190f0a16b43
borisdvlpr <eduffmiranda@hotmail.com>
2025-11-17 23:32:51 +0000

    build: add mapstruct dependency with lombok binding

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,27 @@ repositories {
 }
 
 dependencies {
+    // spring boot
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // mapstruct
+    implementation("org.mapstruct:mapstruct:1.6.3")
+
+    // postgresql
     runtimeOnly("org.postgresql:postgresql:42.7.8")
-    implementation("org.projectlombok:lombok:1.18.42")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("com.h2database:h2:2.3.232")
