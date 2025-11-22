c89d77f2130a89bd2f9d69853dbf08ea1d1e586d
borisdvlpr <eduffmiranda@hotmail.com>
2025-11-22 17:35:26 +0000

    feat(domain): create ticket validation entity class

---
+++ src/main/java/com/borisdvlpr/ticketline/domain/entity/TicketValidation.java
@@ -0,0 +1,45 @@
+package com.borisdvlpr.ticketline.domain.entity;
+
+import com.borisdvlpr.ticketline.domain.TicketValidationMethodEnum;
+import com.borisdvlpr.ticketline.domain.TicketValidationStatusEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ticket_validations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TicketValidation {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationStatusEnum status;
+
+    @Column(name = "validation_method", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationMethodEnum validationMethod;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
