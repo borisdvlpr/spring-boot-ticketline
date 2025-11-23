7b1fce34581f9c81b6edc8f69461df00d1fd5cd4
borisdvlpr <eduffmiranda@hotmail.com>
2025-11-23 23:45:07 +0000

    feat(repository): create user repository

---
+++ src/main/java/com/borisdvlpr/ticketline/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.borisdvlpr.ticketline.repository;
+
+import com.borisdvlpr.ticketline.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
