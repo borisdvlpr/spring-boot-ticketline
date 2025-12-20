b904e9e71d041c38144907e40b0d17408b6a6755
borisdvlpr <eduffmiranda@hotmail.com>
2025-12-20 13:01:24 +0000

    feat(mapper): add mappers for Event to ListEventResponseDTO and TicketType to ListEventTicketTypeResponseDto

--- src/main/java/com/borisdvlpr/ticketline/mapper/EventMapper.java
+++ src/main/java/com/borisdvlpr/ticketline/mapper/EventMapper.java
@@ -2,10 +2,9 @@ package com.borisdvlpr.ticketline.mapper;
 
 import com.borisdvlpr.ticketline.domain.CreateEventRequest;
 import com.borisdvlpr.ticketline.domain.CreateTicketTypeRequest;
-import com.borisdvlpr.ticketline.domain.dto.CreateEventRequestDto;
-import com.borisdvlpr.ticketline.domain.dto.CreateEventResponseDto;
-import com.borisdvlpr.ticketline.domain.dto.CreateTicketTypeRequestDto;
+import com.borisdvlpr.ticketline.domain.dto.*;
 import com.borisdvlpr.ticketline.domain.entity.Event;
+import com.borisdvlpr.ticketline.domain.entity.TicketType;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -16,4 +15,8 @@ public interface EventMapper {
     CreateEventRequest fromDto(CreateEventRequestDto dto);
 
     CreateEventResponseDto toDto(Event event);
+
+    ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventResponseDto(Event event);
 }
