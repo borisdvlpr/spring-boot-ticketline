c5340b86f3b3f69306a5a2e3eb65410d6e5e9a09
borisdvlpr <eduffmiranda@hotmail.com>
2025-12-26 21:40:28 +0000

    feat(domain): add GetEventDetailsResponseDto

---
+++ src/main/java/com/borisdvlpr/ticketline/domain/dto/GetEventDetailsResponseDto.java
@@ -0,0 +1,29 @@
+package com.borisdvlpr.ticketline.domain.dto;
+
+import com.borisdvlpr.ticketline.domain.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetEventDetailsResponseDto {
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    List<GetEventTicketTypesResponseDto> ticketTypes = new ArrayList<>();
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
