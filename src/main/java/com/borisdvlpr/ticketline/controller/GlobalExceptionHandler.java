b1446de3c7e2c553edb933e712d86ad817b7b5a4
borisdvlpr <eduffmiranda@hotmail.com>
2025-12-28 00:30:03 +0000

    feat(controller): update GlobalExceptionHandler to handle new custom exception class

--- src/main/java/com/borisdvlpr/ticketline/controller/GlobalExceptionHandler.java
+++ src/main/java/com/borisdvlpr/ticketline/controller/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.borisdvlpr.ticketline.controller;
 
 import com.borisdvlpr.ticketline.domain.dto.ErrorDto;
+import com.borisdvlpr.ticketline.exception.EventNotFoundException;
+import com.borisdvlpr.ticketline.exception.EventUpdateException;
+import com.borisdvlpr.ticketline.exception.TicketTypeNotFoundException;
 import com.borisdvlpr.ticketline.exception.UserNotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +24,34 @@ public class GlobalExceptionHandler {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setError("User not found.");
 
-        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST );
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleEventNotFoundException(EventNotFoundException ex) {
+        log.error("Caught EventNotFoundException:", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Event not found.");
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EventUpdateException.class)
+    public ResponseEntity<ErrorDto> handleEventUpdateException(EventUpdateException ex) {
+        log.error("Caught EventUpdateException:", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Unable to update event.");
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TicketTypeNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleTicketTypeNotFoundException(TicketTypeNotFoundException ex) {
+        log.error("Caught TicketTypeNotFoundException:", ex);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("Ticket type not found.");
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
