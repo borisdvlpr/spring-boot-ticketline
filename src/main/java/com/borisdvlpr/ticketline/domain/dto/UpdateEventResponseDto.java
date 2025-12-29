2815b3f5474217f295853c15eb0d25b32095a017
borisdvlpr <eduffmiranda@hotmail.com>
2025-12-29 19:16:17 +0000

    feat(domain): add UpdateEventResponseDto

---
+++ src/main/java/com/borisdvlpr/ticketline/domain/dto/UpdateEventResponseDto.java
@@ -0,0 +1,27 @@
+package com.borisdvlpr.ticketline.domain.dto;
+
+import com.borisdvlpr.ticketline.domain.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateEventResponseDto {
+    private UUID uuid;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<UpdateTicketTypeResponseDto> ticketTypes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
