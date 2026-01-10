6cbf8ef42c0ac54d406a7f33d5543813742de832
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-10 17:10:04 +0000

    feat(repository): add findByStatus method to EventRepository

--- src/main/java/com/borisdvlpr/ticketline/repository/EventRepository.java
+++ src/main/java/com/borisdvlpr/ticketline/repository/EventRepository.java
@@ -1,6 +1,7 @@
 package com.borisdvlpr.ticketline.repository;
 
 import com.borisdvlpr.ticketline.domain.entity.Event;
+import com.borisdvlpr.ticketline.domain.type.EventStatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,6 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
 
     Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
+
+    Page<Event> findByStatus(EventStatusEnum status, Pageable pageable);
 }
