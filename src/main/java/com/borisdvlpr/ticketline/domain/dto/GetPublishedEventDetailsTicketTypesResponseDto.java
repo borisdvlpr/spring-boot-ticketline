b79a9bf92aaa91d61b13871efbc46ee49034b69b
borisdvlpr <eduffmiranda@hotmail.com>
2026-01-11 11:43:44 +0000

    feat(domain): add GetPublishedEventDetailsTicketTypesResponseDto

---
+++ src/main/java/com/borisdvlpr/ticketline/domain/dto/GetPublishedEventDetailsTicketTypesResponseDto.java
@@ -0,0 +1,10 @@
+package com.borisdvlpr.ticketline.domain.dto;
+
+import java.util.UUID;
+
+public class GetPublishedEventDetailsTicketTypesResponseDto {
+    private UUID id;
+    private String name;
+    private Double price;
+    private String description;
+}
