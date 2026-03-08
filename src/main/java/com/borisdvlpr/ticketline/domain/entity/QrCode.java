5f7337f7f321b1d27ff039ec54f00be0270285fb
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-08 22:15:37 +0000

    refactor(domain): remove auto generation of uuid for QrCore entity

--- src/main/java/com/borisdvlpr/ticketline/domain/entity/QrCode.java
+++ src/main/java/com/borisdvlpr/ticketline/domain/entity/QrCode.java
@@ -20,7 +20,6 @@ import java.util.UUID;
 public class QrCode {
     @Id
     @Column(name = "id", nullable = false, updatable = false)
-    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "status", nullable = false)
