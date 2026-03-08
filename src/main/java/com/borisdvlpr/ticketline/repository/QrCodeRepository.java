dbf74e2648403790ba67316bce5acb3b9b7d48cc
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-08 22:54:06 +0000

    feat(repository): add QrCode repository

---
+++ src/main/java/com/borisdvlpr/ticketline/repository/QrCodeRepository.java
@@ -0,0 +1,12 @@
+package com.borisdvlpr.ticketline.repository;
+
+import com.borisdvlpr.ticketline.domain.entity.QrCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+
+}
