74314ee3338f5beec173ac978244ed83a3909020
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-10 19:20:26 +0000

    feat(service): add getQrCodeImageForUserAndTicket method to QrCodeService and implementation in QrCodeServiceImpl

--- src/main/java/com/borisdvlpr/ticketline/service/QrCodeService.java
+++ src/main/java/com/borisdvlpr/ticketline/service/QrCodeService.java
@@ -3,6 +3,10 @@ package com.borisdvlpr.ticketline.service;
 import com.borisdvlpr.ticketline.domain.entity.QrCode;
 import com.borisdvlpr.ticketline.domain.entity.Ticket;
 
+import java.util.UUID;
+
 public interface QrCodeService {
     QrCode generateQrCode(Ticket ticket);
+
+    byte[] getQrCodeImageForUserAndTicket(UUID ticketId, UUID userId);
 }
--- src/main/java/com/borisdvlpr/ticketline/service/impl/QrCodeServiceImpl.java
+++ src/main/java/com/borisdvlpr/ticketline/service/impl/QrCodeServiceImpl.java
@@ -4,6 +4,7 @@ import com.borisdvlpr.ticketline.domain.entity.QrCode;
 import com.borisdvlpr.ticketline.domain.entity.Ticket;
 import com.borisdvlpr.ticketline.domain.type.QrCodeStatusEnum;
 import com.borisdvlpr.ticketline.exception.QrCodeGenerationException;
+import com.borisdvlpr.ticketline.exception.QrCodeNotFoundException;
 import com.borisdvlpr.ticketline.repository.QrCodeRepository;
 import com.borisdvlpr.ticketline.service.QrCodeService;
 import com.google.zxing.BarcodeFormat;
@@ -12,6 +13,8 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.web.embedded.TomcatWebServerFactoryCustomizer;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
@@ -21,6 +24,7 @@ import java.io.IOException;
 import java.util.Base64;
 import java.util.UUID;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class QrCodeServiceImpl implements QrCodeService {
@@ -29,6 +33,7 @@ public class QrCodeServiceImpl implements QrCodeService {
 
     private final QrCodeRepository qrCodeRepository;
     private final QRCodeWriter qrCodeWriter;
+    private final TomcatWebServerFactoryCustomizer tomcatWebServerFactoryCustomizer;
 
     @Override
     public QrCode generateQrCode(Ticket ticket) {
@@ -46,6 +51,20 @@ public class QrCodeServiceImpl implements QrCodeService {
 
         } catch (WriterException | IOException e) {
             throw new QrCodeGenerationException("Failed to generate QR code", e);
+        }
+    }
+
+    @Override
+    public byte[] getQrCodeImageForUserAndTicket(UUID ticketId, UUID userId) {
+        QrCode qrCode = qrCodeRepository.findByTicketIdAndTicketPurchaserId(ticketId, userId)
+                .orElseThrow(QrCodeNotFoundException::new);
+
+        try {
+            return Base64.getDecoder().decode(qrCode.getValue());
+
+        } catch (IllegalArgumentException e) {
+            log.error("Ivalid base64 QR code for ticket ID: {}", ticketId, e);
+            throw new QrCodeNotFoundException();
         }
     }
 
