b0dbea110bd2a4b0234d989d6a9002c8819f7612
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-10 18:56:58 +0000

    feat(service): add getTicketForUser method to TicketService and implementation in TicketServiceImpl

--- src/main/java/com/borisdvlpr/ticketline/service/TicketService.java
+++ src/main/java/com/borisdvlpr/ticketline/service/TicketService.java
@@ -4,8 +4,11 @@ import com.borisdvlpr.ticketline.domain.entity.Ticket;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TicketService {
     Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable);
+
+    Optional<Ticket> getTicketForUser(UUID ticketId, UUID userId);
 }
