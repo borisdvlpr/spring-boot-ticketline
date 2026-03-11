d43e005b8e43fb28540f2522df8065e66c30db64
borisdvlpr <eduffmiranda@hotmail.com>
2026-03-11 19:05:53 +0000

    feat(repository): add findByIdAndStatus method to QrCodeRepository

--- src/main/java/com/borisdvlpr/ticketline/repository/QrCodeRepository.java
+++ src/main/java/com/borisdvlpr/ticketline/repository/QrCodeRepository.java
@@ -1,6 +1,7 @@
 package com.borisdvlpr.ticketline.repository;
 
 import com.borisdvlpr.ticketline.domain.entity.QrCode;
+import com.borisdvlpr.ticketline.domain.type.QrCodeStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.UUID;
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
     Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID purchaserId);
+
+    Optional<QrCode> findByIdAndStatus(UUID id, QrCodeStatusEnum status);
 }
